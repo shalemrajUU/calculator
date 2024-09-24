b694e15c07cdbcf2d68bc1d4a203b78d5840d3e8
Shalem Raj Bogiri <Bogiri-SR@ulster.ac.uk>
2024-09-24 15:43:55 +0100

    new program

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//calc
+
+//user input 
+
+Console.WriteLine("input first number");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input second number");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstnumber + secondnumber;
+
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstnumber, secondnumber, result);
+
+
+
+
